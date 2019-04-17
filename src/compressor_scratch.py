9f1f7ebf783ff07e343847b3ddfd195c3a002acb
Bernardo Bahia Monteiro <b.b.monteiro@gmail.com>
2019-04-16 22:23:33 -0300

    Added incidence and skin friction losses

--- src/compressor_scratch.py
+++ src/compressor_scratch.py
@@ -1,7 +1,7 @@
 from math import tan, pi
 from warnings import warn
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 from mfp2mach import mfp2mach, mach2mfp
 
@@ -26,13 +26,6 @@ def phi2(MFP2_, Mrotor, gam, T0_ratio):
     return phi2_
 
 
-def psi_euler(phi2, beta2, slip_factor):
-    
-    psi = slip_factor*(1-phi2*tan(beta2))
-    
-    return psi
-
-
 def psi2T0_ratio(psi_actual, Mrotor, gam):
     return 1+ psi_actual*Mrotor*2*(gam-1)
 
@@ -40,7 +33,7 @@ def psi2T0_ratio(psi_actual, Mrotor, gam):
 def psi2P0_ratio(psi_isen, Mrotor, gam):
     return psi2T0_ratio(psi_isen, Mrotor, gam)**(gam/(gam-1))
 
-def compressor_dimensionless(MFP, Mrotor, gam, beta2, A_ratio):
+def compressor_dimensionless(MFP, Mrotor, gam, beta1, beta2, A_ratio, R_ratio):
     slip_factor = 0.9
 
     # guess P0_ratio and T0_ratio
@@ -49,18 +42,35 @@ def compressor_dimensionless(MFP, Mrotor, gam, beta2, A_ratio):
 
     tol = 1e-8
     iterations = 0
+    considering_losses = False
     while True:
         MFP2_ = MFP2(MFP, gam, A_ratio, P0_ratio, T0_ratio)
         phi2_ = phi2(MFP2_, Mrotor, gam, T0_ratio)
+        phi1_ = phi1(MFP, Mrotor, gam)
 
-        psi = psi_euler(phi2_, beta2, slip_factor)
+        psi_euler = slip_factor*(1-phi2_*tan(beta2))
+        
+        psi_isen = psi_euler
 
-        P0_ratio_new = psi2P0_ratio(psi, Mrotor, gam)
-        T0_ratio_new = psi2T0_ratio(psi, Mrotor, gam)
+        if considering_losses:
+            pass
+            psi_isen = psi_isen - (
+                incidence_loss(phi1_, beta1, R_ratio)
+              + skin_friction_loss(0.004, 3, slip_factor, beta2, phi1_, phi2_,1/R_ratio , 1/R_ratio)
+              )
+        
+        psi_actual = psi_euler         
+        
+        P0_ratio_new = psi2P0_ratio(psi_isen, Mrotor, gam)
+        T0_ratio_new = psi2T0_ratio(psi_actual, Mrotor, gam)
 
         if    (np.all(np.abs((P0_ratio_new - P0_ratio)/P0_ratio)<=tol)
            and np.all(np.abs((T0_ratio_new - T0_ratio)/T0_ratio)<=tol)):
-            break
+            if considering_losses:
+                break
+            else:
+                considering_losses=True
+                print("Converged without losses, iter={}".format(iterations))
         
         if iterations>=50:
             warn("Exceeded number of iterations")
@@ -76,21 +86,59 @@ def compressor_dimensionless(MFP, Mrotor, gam, beta2, A_ratio):
     print(iterations)
     return P0_ratio, T0_ratio, choked
 
+def plot_map(MFP, M0rotor, gam, beta1, beta2, A_ratio, R_ratio):
+    MFP_grid, M0rotor_grid = np.meshgrid(MFP, M0rotor)
+    P0_ratio, T0_ratio, choked = compressor_dimensionless(MFP_grid, M0rotor_grid, gam, beta1, beta2, A_ratio, R_ratio)
+    T0_ratio_isen = P0_ratio**((gam-1)/gam)
+    eff = np.log(T0_ratio_isen)/np.log(T0_ratio)
 
+    #P0_ratio[choked] = 0
+    #eff[choked] = 0
 
-import matplotlib.pyplot as plt
+    cs_P0 = plt.contour(MFP_grid, P0_ratio, M0rotor_grid)
+    plt.clabel(cs_P0)
+    cs_eff = plt.contour(MFP_grid, P0_ratio, eff, 30)
+    plt.clabel(cs_eff)
+    #cs_choked = plt.contour(MFP_grid, P0_ratio, choked, 1)
+    #print(choked)
 
-print(compressor_dimensionless(0, 0.6, 1.4, 30*pi/180, 1))
+    plt.title("Compressor map")
+    plt.xlabel("MFP")
+    plt.ylabel("P02/P01")
+    plt.legend(("M0rotor", "eta_p"))
+    
+    return P0_ratio, eff
+
+def incidence_loss(phi1_, beta1, R_ratio):
+    
+    beta1_opt = np.arctan(1/(R_ratio*phi1_))
+    delta_psi = ((1/R_ratio)**2+phi1_**2)*np.sin(beta1-beta1_opt)**2
+
+    return delta_psi
+
+def skin_friction_loss(Cf, Lb_Dhyd, slip_factor, beta2, phi1_, phi2_, D1t_D2, D1h_D2):
+    #every speed is adimentionalized by U2
+    V2 = np.sqrt(phi2_**2 + (slip_factor*(1-phi2_*tan(beta2)))**2)
+    W1t = np.sqrt(D1t_D2**2 + phi1_**2)
+    W1h = np.sqrt(D1h_D2**2 + phi1_**2)
+    W2 = phi2_*np.sqrt(1+tan(beta2)**2)
+
+    Wbar = (phi1_ + V2+ W1t + 2*W1h + 3*W2)/8
+
+    return 2*Cf*Lb_Dhyd*Wbar**2
+
 
 slip_factor=0.9
 gam = 1.4
 MFP_choke = mach2mfp(1,gam)
-MFP = np.linspace(0,MFP_choke,1000)
+MFP = np.linspace(0,MFP_choke,100)
 M = mfp2mach(MFP, gam)
-Mrotor = 0.1
-beta2 = 10*pi/180
-A_ratio = 0.8
-P0_ratio, T0_ratio, choked = compressor_dimensionless(MFP, Mrotor, gam, beta2, A_ratio)
+Mrotor = 0.3
+beta2 = 0*pi/180
+beta1 = 40*pi/180
+A_ratio = 2
+R_ratio = 2
+P0_ratio, T0_ratio, choked = compressor_dimensionless(MFP, Mrotor, gam, beta1, beta2, A_ratio, R_ratio)
 MFP2_ = MFP2(MFP, gam, A_ratio, P0_ratio, T0_ratio)
 phi2_ = phi2(MFP2_, Mrotor, gam, T0_ratio)
 phi1_ = phi1(MFP, Mrotor, gam)
@@ -105,6 +153,9 @@ plt.plot(MFP, choked)
 # plt.plot(MFP, M2(MFP, Mrotor, gam, P0_ratio, T0_ratio, A_ratio, slip_factor, beta2))
 plt.hlines(MFP_choke, 0, MFP_choke)
 plt.vlines(MFP_choke, 0, MFP_choke)
+plt.figure()
+P0_ratio, eff = plot_map(MFP, np.linspace(0.3,1.5,100), gam, beta1, beta2, A_ratio, R_ratio)
+
 
 plt.show()
 
@@ -139,4 +190,6 @@ class Compressor:
 
 
 
+#TODO: plot individal losses, return losses in a map, return relevant params (e.g. phi2)
+#Improve visualization of what is happening
 
